fca0cfefd458a8b9f793d52b3341919d02fea8e1
Hyunmin Lim <hmlim@ucsd.edu>
2023-11-27 20:24:56 -0800

    Versioning untracked files

--- portfolio/components/Main.jsx
+++ portfolio/components/Main.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { AiOutlineMail } from 'react-icons/ai';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 
 const Main = () => {
   return (
@@ -6,6 +9,24 @@ const Main = () => {
         <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
             <div>
                 <p className='uppercase text-sm tracking-widest text-gray-600'>LET'S BUILD SOMETHING TOGETHER</p>
+                <h1 className='py-4 text-gray-700'>Hi, I'm <span className='text-[#6629e8]'> Hyunmin</span></h1>
+                <h1 className='py-4 text-gray-700'>A Software Engineer</h1>
+                <p className='py-4 text-gray-600 max-w-[70%] m-auto'>I can be a frontend and backend engineer</p>
+                <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <FaLinkedinIn />
+                    </div>
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <FaGithub />
+                    </div>
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <AiOutlineMail />
+                    </div>
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <BsFillPersonLinesFill />
+                    </div>
+
+                </div>
             </div>
         </div>
     </div>
